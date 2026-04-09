b34ad0cddeb0fd788893f26e7094d81c965c9426
Ashutosh <djashutosh673@gmail.com>
2026-04-09 09:39:25 +0530

    fix: add Program.cs entry point

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure pipeline
+app.MapControllers();
+
+app.Run();
